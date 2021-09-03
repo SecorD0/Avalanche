50bb45763e0f82547288b45d7d9747709833f620
SecorD0 <vovan6573@gmail.com>
2021-09-03 23:54:26 +0300

    Node auto-installation

--- installer.sh
+++ installer.sh
@@ -34,7 +34,7 @@ sudo systemctl restart avalanchd
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "avalanch_log" "sudo journalctl -f -n 100 -u avalanchd" true
 echo -e '\e[40m\e[92mDone!\e[0m'
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
-echo -e '\nThe node was \e[40m\e[92mstarted\e[0m.'
+echo -e '\nThe node was \e[40m\e[92mstarted\e[0m.\n\n'
 echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
 echo -e 'To view the node status: \e[40m\e[92msystemctl status avalanchd\e[0m'
 echo -e 'To view the node log: \e[40m\e[92mavalanch_log\e[0m'
